d6b72077a7bc72cd0cbbf369db1ff729a8875e70
Curtis Robinson <curtisrobinson2303@gmail.com>
2023-09-24 16:37:19 -0700

    all files as of sept 24

---
+++ app.js
@@ -0,0 +1,22 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+// Function to close the menu
+function closeMenu() {
+  menu.classList.remove('is-active');
+  menuLinks.classList.remove('active');
+}
+
+// Event listener to close the menu when the user scrolls
+window.addEventListener('scroll', closeMenu);
+
+// Event listener to change the state of the menu item when clicked
+menu.addEventListener('click', function() {
+    if (menu.classList.contains('is-active')) {
+      menu.classList.remove('is-active');
+    } else {
+      menu.classList.add('is-active');
+    }
+    menuLinks.classList.toggle('active');
+  });
+  
